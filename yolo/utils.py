5a205cbc5617415e22247f5503573eb801383804
JakubSzwajka <szwajkajakub@gmail.com>
2021-02-10 11:16:56 +0100

    cleaning

--- yolo/utils.py
+++ yolo/utils.py
@@ -23,3 +23,17 @@ def determinate_total_frames(video_path):
 
     return total
 
+def convert_boxes(image, boxes):
+    returned_boxes = []
+    for box in boxes:
+        box[0] = (box[0] * image.shape[1]).astype(int)
+        box[1] = (box[1] * image.shape[0]).astype(int)
+        box[2] = (box[2] * image.shape[1]).astype(int)
+        box[3] = (box[3] * image.shape[0]).astype(int)
+        box[2] = int(box[2]-box[0])
+        box[3] = int(box[3]-box[1])
+        box = box.astype(int)
+        box = box.tolist()
+        if box != [0,0,0,0]:
+            returned_boxes.append(box)
+    return returned_boxes
